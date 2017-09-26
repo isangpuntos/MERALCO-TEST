1029b6b6f0c5f6d8ed3e3113665562473fd6985c
isangpuntos <isang.puntos@gmail.com>
2017-09-26 19:39:44 +0800

    Get Request format
    Get Request format

--- index.js
+++ index.js
@@ -78,107 +78,104 @@ restService.post('/webhook', function(req, res) {
 });
 
 restService.post('/testFrom', function(req, res) {
-var from = ["Bacolod", "Bato, Leyte", "CDO", "Cebu", "Camotes", "Dipolog", "Iligan", "Ilo-ilo"]; 
+    console.log(res);
+    var from = ["Bacolod", "Bato, Leyte", "CDO", "Cebu", "Camotes", "Dipolog", "Iligan", "Ilo-ilo"]; 
 
-var fromList = [];
-for (var i = 0; i < from.length; i++) {
-     fromList.push({          
-	      "title": from[i],
-              "block_names": ["To2"],
-              "set_attributes": 
-              {
-                "travel_from": from[i]
-              },
-	 });
-} 
+    var fromList = [];
+    for (var i = 0; i < from.length; i++) {
+         fromList.push({          
+              "title": from[i],
+                  "block_names": ["To2"],
+                  "set_attributes": 
+                  {
+                    "travel_from": from[i]
+                  },
+         });
+    } 
 
-fromList.push({          
-     "title": "NEXT>>",
-     "block_names": ["NEXT>>"],
-     "set_attributes": 
-      {
-          "travel_from": "NEXT>>"
-      },
-});
+    fromList.push({          
+         "title": "NEXT>>",
+         "block_names": ["NEXT>>"],
+         "set_attributes": 
+          {
+              "travel_from": "NEXT>>"
+          },
+    });
 
-var fromJson = {
-				"messages": [
-					{
-					    "attachment": {
-						"type": "template",
-						"payload": {
-							"template_type": "button",
-							"text": "Travel from?",
-							"buttons": [
-							{
-								"type": "show_block",
-								"block_names": ["Search"],
-								"title": "Back"
-							}
-						  ]
-						}
-					  },
-					  "quick_replies": ""
-					}
-				]
-			};
+    var fromJson = {
+                    "messages": [
+                        {
+                            "attachment": {
+                            "type": "template",
+                            "payload": {
+                                "template_type": "button",
+                                "text": "Travel from?",
+                                "buttons": [
+                                {
+                                    "type": "show_block",
+                                    "block_names": ["Search"],
+                                    "title": "Back"
+                                }
+                              ]
+                            }
+                          },
+                          "quick_replies": ""
+                        }
+                    ]
+                };
 
-fromJson.messages[0].quick_replies = fromList;
-console.log(fromJson.messages[0]);
-res.send(fromJson);
-console.log(fromJson);
+    fromJson.messages[0].quick_replies = fromList;
+    res.send(fromJson);
 });
 
 restService.post('/testTo', function(req, res) {
-var toArr = ["Camotes", "Iligan"]; 
+    var toArr = ["Camotes", "Iligan"]; 
 
-var toList = [];
-for (var i = 0; i < toArr.length; i++) {
-     toList.push({          
-	      "title": toArr[i],
-              "block_names": ["Date2"],
-              "set_attributes": 
-              {
-                "travel_to": toArr[i]
-              },
-	 });
-} 
+    var toList = [];
+    for (var i = 0; i < toArr.length; i++) {
+         toList.push({          
+              "title": toArr[i],
+                  "block_names": ["Date2"],
+                  "set_attributes": 
+                  {
+                    "travel_to": toArr[i]
+                  },
+         });
+    } 
 
-/*fromList.push({          
-     "title": "NEXT>>",
-     "block_names": ["NEXT>>"],
-     "set_attributes": 
-      {
-          "travel_from": "NEXT>>"
-      },
-});*/
+    /*fromList.push({          
+         "title": "NEXT>>",
+         "block_names": ["NEXT>>"],
+         "set_attributes": 
+          {
+              "travel_from": "NEXT>>"
+          },
+    });*/
 
-var toJson = {
-				"messages": [
-					{
-					    "attachment": {
-						"type": "template",
-						"payload": {
-							"template_type": "button",
-							"text": "Travel to?",
-							"buttons": [
-							{
-								"type": "show_block",
-								"block_names": ["Search"],
-								"title": "Back"
-							}
-						  ]
-						}
-					  },
-					  "quick_replies": ""
-					}
-				]
-			};
+    var toJson = {
+                    "messages": [
+                        {
+                            "attachment": {
+                            "type": "template",
+                            "payload": {
+                                "template_type": "button",
+                                "text": "Travel to?",
+                                "buttons": [
+                                {
+                                    "type": "show_block",
+                                    "block_names": ["Search"],
+                                    "title": "Back"
+                                }
+                              ]
+                            }
+                          },
+                          "quick_replies": ""
+                        }
+                    ]
+                };
 
-toJson.messages[0].quick_replies = toList;
-console.log(toJson.messages[0]);
-res.send(toJson);
-console.log(toJson);
+    toJson.messages[0].quick_replies = toList;
+    res.send(toJson);
 });
 
 restService.listen((process.env.PORT || 8000), function() {
